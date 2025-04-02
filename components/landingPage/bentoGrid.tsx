36a0685c117c05d8abe1bcc631bc5448962440a6
PEDRO LUCAS SANTANA <80850842+pedrolucas12@users.noreply.github.com>
2025-04-01 22:18:28 -0300

    add utility functions and components; implement BentoGrid and BentoCard for enhanced layout and styling

--- components/landingPage/bentoGrid.tsx
+++ components/landingPage/bentoGrid.tsx
@@ -1,40 +1,92 @@
-export default function BentoGrid() {
+import { CalendarIcon, FileTextIcon } from "@radix-ui/react-icons";
+import { BellIcon, Share2Icon } from "lucide-react";
+import { BentoCard, BentoGrid } from "../magicui/bento-grid";
+
+const features = [
+  {
+    Icon: FileTextIcon,
+    name: "Cálculo de TMB",
+    description: "Descubra sua taxa metabólica basal com precisão.",
+    href: "#",
+    cta: "Saiba mais",
+    className: "col-span-3 lg:col-span-1",
+    background: (
+      <p>teste</p>
+      // <Marquee
+      //   pauseOnHover
+      //   className="absolute top-10 [--duration:20s] [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)]"
+      // >
+      //   <div className="relative w-32 cursor-pointer overflow-hidden rounded-xl border p-4 bg-gray-100">
+      //     <p className="text-sm font-medium">Taxa Metabólica Basal</p>
+      //     <blockquote className="mt-2 text-xs">
+      //       Saiba como calcular a energia que seu corpo precisa para funcionar.
+      //     </blockquote>
+      //   </div>
+      // </Marquee>
+    ),
+  },
+  {
+    Icon: BellIcon,
+    name: "Percentual de Gordura",
+    description: "Saiba como seu corpo está em relação à saúde.",
+    href: "#",
+    cta: "Saiba mais",
+    className: "col-span-3 lg:col-span-2",
+    background: (
+      <div className="absolute right-2 top-4 h-[300px] w-full scale-75 border-none transition-all duration-300 ease-out">
+        <p className="text-sm font-medium">Percentual de Gordura</p>
+        <blockquote className="mt-2 text-xs">
+          Descubra como calcular a composição corporal com precisão.
+        </blockquote>
+      </div>
+    ),
+  },
+  {
+    Icon: Share2Icon,
+    name: "Macros Personalizados",
+    description: "Receba recomendações de proteínas, carboidratos e gorduras.",
+    href: "#",
+    cta: "Saiba mais",
+    className: "col-span-3 lg:col-span-2",
+    background: (
+      <div className="absolute right-2 top-4 h-[300px] border-none transition-all duration-300 ease-out">
+        <p className="text-sm font-medium">Macros Personalizados</p>
+        <blockquote className="mt-2 text-xs">
+          Ajuste sua dieta com base nas suas necessidades diárias.
+        </blockquote>
+      </div>
+    ),
+  },
+  {
+    Icon: CalendarIcon,
+    name: "Dieta Ajustada",
+    description: "Um plano alimentar feito exclusivamente para você.",
+    href: "#",
+    cta: "Saiba mais",
+    className: "col-span-3 lg:col-span-1",
+    background: (
+      <div className="absolute right-0 top-10 origin-top scale-75 rounded-md border transition-all duration-300 ease-out">
+        <p className="text-sm font-medium">Dieta Ajustada</p>
+        <blockquote className="mt-2 text-xs">
+          Receba um plano alimentar personalizado para seus objetivos.
+        </blockquote>
+      </div>
+    ),
+  },
+];
+
+export default function BentoGridSection() {
   return (
     <section className="py-20">
       <div className="container mx-auto px-6">
         <h2 className="text-3xl font-bold text-center mb-10">
           O que o NutriCalc faz por você
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          <div className="bg-white shadow-lg rounded-lg p-6 text-center">
-            <h3 className="text-xl font-semibold mb-4">Cálculo de TMB</h3>
-            <p className="text-gray-600">
-              Descubra sua taxa metabólica basal com precisão.
-            </p>
-          </div>
-          <div className="bg-white shadow-lg rounded-lg p-6 text-center">
-            <h3 className="text-xl font-semibold mb-4">
-              Percentual de Gordura
-            </h3>
-            <p className="text-gray-600">
-              Saiba como seu corpo está em relação à saúde.
-            </p>
-          </div>
-          <div className="bg-white shadow-lg rounded-lg p-6 text-center">
-            <h3 className="text-xl font-semibold mb-4">
-              Macros Personalizados
-            </h3>
-            <p className="text-gray-600">
-              Receba recomendações de proteínas, carboidratos e gorduras.
-            </p>
-          </div>
-          <div className="bg-white shadow-lg rounded-lg p-6 text-center">
-            <h3 className="text-xl font-semibold mb-4">Dieta Ajustada</h3>
-            <p className="text-gray-600">
-              Um plano alimentar feito exclusivamente para você.
-            </p>
-          </div>
-        </div>
+        <BentoGrid>
+          {features.map((feature, idx) => (
+            <BentoCard key={idx} {...feature} />
+          ))}
+        </BentoGrid>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,74 +1,114 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: [
+    darkMode: ['class'],
+    content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        dark_green: {
-          DEFAULT: '#12372a',
-          100: '#030b08',
-          200: '#071610',
-          300: '#0a2019',
-          400: '#0e2b21',
-          500: '#12372a',
-          600: '#27785c',
-          700: '#3cba8e',
-          800: '#7bd4b5',
-          900: '#bdeada',
-        },
-        honeydew: {
-          DEFAULT: '#ebf5df',
-          100: '#314717',
-          200: '#638f2d',
-          300: '#93c851',
-          400: '#bfdf99',
-          500: '#ebf5df',
-          600: '#f0f7e6',
-          700: '#f3f9ec',
-          800: '#f7fbf3',
-          900: '#fbfdf9',
-        },
-        moss_green: {
-          DEFAULT: '#768948',
-          100: '#181b0e',
-          200: '#2f371d',
-          300: '#47522b',
-          400: '#5e6e3a',
-          500: '#768948',
-          600: '#96ac61',
-          700: '#b0c189',
-          800: '#cad5b0',
-          900: '#e5ead8',
-        },
-        dim_gray: {
-          DEFAULT: '#6a687a',
-          100: '#151518',
-          200: '#2a2930',
-          300: '#3f3e49',
-          400: '#545361',
-          500: '#6a687a',
-          600: '#868496',
-          700: '#a4a2b0',
-          800: '#c2c1ca',
-          900: '#e1e0e5',
-        },
-        primary: {
-          DEFAULT: '#12372a', // dark_green
-          light: '#7bd4b5',   // Variante mais clara de dark_green
-          dark: '#030b08',    // Variante mais escura de dark_green
-        },
-        secondary: {
-          DEFAULT: '#ebf5df', // honeydew
-          light: '#f7fbf3',   // Variante mais clara de honeydew
-          dark: '#314717',    // Variante mais escura de honeydew
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			dark_green: {
+  				'100': '#030b08',
+  				'200': '#071610',
+  				'300': '#0a2019',
+  				'400': '#0e2b21',
+  				'500': '#12372a',
+  				'600': '#27785c',
+  				'700': '#3cba8e',
+  				'800': '#7bd4b5',
+  				'900': '#bdeada',
+  				DEFAULT: '#12372a'
+  			},
+  			honeydew: {
+  				'100': '#314717',
+  				'200': '#638f2d',
+  				'300': '#93c851',
+  				'400': '#bfdf99',
+  				'500': '#ebf5df',
+  				'600': '#f0f7e6',
+  				'700': '#f3f9ec',
+  				'800': '#f7fbf3',
+  				'900': '#fbfdf9',
+  				DEFAULT: '#ebf5df'
+  			},
+  			moss_green: {
+  				'100': '#181b0e',
+  				'200': '#2f371d',
+  				'300': '#47522b',
+  				'400': '#5e6e3a',
+  				'500': '#768948',
+  				'600': '#96ac61',
+  				'700': '#b0c189',
+  				'800': '#cad5b0',
+  				'900': '#e5ead8',
+  				DEFAULT: '#768948'
+  			},
+  			dim_gray: {
+  				'100': '#151518',
+  				'200': '#2a2930',
+  				'300': '#3f3e49',
+  				'400': '#545361',
+  				'500': '#6a687a',
+  				'600': '#868496',
+  				'700': '#a4a2b0',
+  				'800': '#c2c1ca',
+  				'900': '#e1e0e5',
+  				DEFAULT: '#6a687a'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				light: '#7bd4b5',
+  				dark: '#030b08',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				light: '#f7fbf3',
+  				dark: '#314717',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 
 module.exports = config;
